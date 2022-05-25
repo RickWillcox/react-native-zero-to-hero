b53f86c91754c2ac41499be6e3befa64894ad213
Rick Willcox <rickwillcox90@gmail.com>
2022-05-25 15:13:20 +1000

    flex boxes 1:13

--- App.js
+++ App.js
@@ -1,51 +1,41 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button} from 'react-native';
-import { Linking } from 'react-native-web';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Linking, YellowBox } from 'react-native-web';
 
 export default function App() {
-  const [name, setName] = useState('Rick');
-  const [session, setSession] = useState({number: 0, title: 'state'});
-  const [current, setCurrent] = useState(true);
-  const [count, setCount] = useState({count: 0, clicked: 0});
-
-  const changeName = () => {
-    if (name === 'Rick') {
-      setName('Morty');
-    } else{
-      setName('Rick');
-    }
-  }
-
-  const incrementCounter = () => {
-    setCount({count: count.count + 5, clicked: count.clicked + 1});
-  }
-
-  const resetCounter = () => {
-    setCount({count: 0, clicked: 0});
-  }
-
-  const incrementSession = () => {
-    setSession({number: session.number + 1, title: 'session'});
-    setCurrent(false);
-  }
-  
-
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>My name is {name}</Text>  
-      <Text style={styles.text}>The session number is {session.number}</Text>  
-      <Text style={styles.text}>{current ? 'current session' : 'next session'}</Text>  
-      <Button title='Change name' onPress={changeName}> </Button>
-      <Button title='Increment session' onPress={incrementSession}> </Button>
-      <Text style={styles.text}>Total Count: {count.count}</Text>
-      <Text style={styles.text}>Total Clicks: {count.clicked}</Text>
-      <Button title='Increment Counter' onPress={incrementCounter}> </Button>
-      <Button title='Reset Counter' onPress={resetCounter}> </Button>
+      <View style={styles.body1}>
+        <View style={styles.view1}>
+          <Text>1</Text>
+        </View>
+        <View style={styles.view2}>
+          <Text>2</Text>
+        </View>
+        <View style={styles.view3}>
+          <Text>3</Text>
+        </View>
+      </View>
 
+      <View style={styles.body2}>
+        <View style={styles.view4}>
+          <Text>4</Text>
+        </View>
+        <View style={styles.view5}>
+          <Text>5</Text>
+        </View>
+      </View>
 
-
+      <View style={styles.body3}>
+        <View style={styles.view6}>
+          <Text>6</Text>
+        </View>
+        <View style={styles.view7}>
+          <Text>7</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -53,15 +43,79 @@ export default function App() {
 const styles = StyleSheet.create({
   body: {
     flex: 1,
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  body1: {
+    flex: 1,
+    flexDirection: 'row',
     backgroundColor: '#0000FF',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  body2: {
+    flex: 2,
+    flexDirection: 'column',
+    backgroundColor: '#0000FF',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  body3: {
+    flex: 7,
+    flexDirection: 'row',
+    backgroundColor: '#0000FF',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  view1: {
+    flex: 1,
+    backgroundColor: '#1ff6d0',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  view2: {
+    flex: 2,
+    backgroundColor: '#FF00FF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view3: {
+    flex: 3,
+    backgroundColor: '#FFFF00',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view4: {
+    flex: 1,
+    backgroundColor: '#ef0707',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view5: {
+    flex: 1,
+    backgroundColor: '#75f61f',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view6: {
+    flex: 3,
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view7: {
+    flex: 3,
+    backgroundColor: '#2919e8',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
   text: {
-    fontSize: 30,
+    fontSize: 25,
     fontWeight: 'bold',
     fontFamily: 'montserrat',
     color: 'white',
     margin: 10,
-  }
+  },
 });
