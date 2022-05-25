f7d12e9fd0e5c7d44cd4d50d624f05d0d6a695a8
Rick Willcox <rickwillcox90@gmail.com>
2022-05-25 13:50:44 +1000

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import { Linking } from 'react-native-web';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.body}>
+      <Text style={styles.text}>Hello World</Text>  
+      <Button title='Zero to Hero' style={styles.text} onPress={()=>{Linking.openURL('https://www.google.com')}}> </Button>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  body: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0000FF',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    fontFamily: 'montserrat',
+    color: 'white',
+    margin: 10,
+  }
 });
