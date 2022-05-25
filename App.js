a3d214fb43915a55660a801aa45d9b0abbce79e1
Rick Willcox <rickwillcox90@gmail.com>
2022-05-25 14:05:17 +1000

    counter working

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import { Linking } from 'react-native-web';
 export default function App() {
   const [name, setName] = useState('Rick');
   const [session, setSession] = useState({number: 0, title: 'state'});
+  const [current, setCurrent] = useState(true);
+  const [count, setCount] = useState({count: 0, clicked: 0});
 
   const changeName = () => {
     if (name === 'Rick') {
@@ -16,8 +18,17 @@ export default function App() {
     }
   }
 
+  const incrementCounter = () => {
+    setCount({count: count.count + 5, clicked: count.clicked + 1});
+  }
+
+  const resetCounter = () => {
+    setCount({count: 0, clicked: 0});
+  }
+
   const incrementSession = () => {
     setSession({number: session.number + 1, title: 'session'});
+    setCurrent(false);
   }
   
 
@@ -25,8 +36,16 @@ export default function App() {
     <View style={styles.body}>
       <Text style={styles.text}>My name is {name}</Text>  
       <Text style={styles.text}>The session number is {session.number}</Text>  
+      <Text style={styles.text}>{current ? 'current session' : 'next session'}</Text>  
       <Button title='Change name' onPress={changeName}> </Button>
       <Button title='Increment session' onPress={incrementSession}> </Button>
+      <Text style={styles.text}>Total Count: {count.count}</Text>
+      <Text style={styles.text}>Total Clicks: {count.clicked}</Text>
+      <Button title='Increment Counter' onPress={incrementCounter}> </Button>
+      <Button title='Reset Counter' onPress={resetCounter}> </Button>
+
+
+
     </View>
   );
 }
